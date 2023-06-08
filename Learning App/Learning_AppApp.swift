4403b26c0d427215deba69a1cf7682f3bcc64143
Andrew Koo <andrewkykoo@gmail.com>
2023-06-08 12:25:43 -0400

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by Andrew Koo on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
