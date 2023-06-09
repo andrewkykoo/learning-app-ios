e8a2ddcf24191bff5394f408f9a4758035a53266
Andrew Koo <andrewkykoo@gmail.com>
2023-06-08 22:36:09 -0400

    create ContentModel

---
+++ Learning App/Models/Models.swift
@@ -0,0 +1,46 @@
+//
+//  Models.swift
+//  Learning App
+//
+//  Created by Andrew Koo on 6/8/23.
+//
+
+import Foundation
+
+struct Module: Decodable {
+    var id: Int
+    var category: String
+    var content: Content
+    var test: Test
+}
+
+struct Content: Decodable {
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var lessons: [Lesson]
+}
+
+struct Lesson: Decodable {
+    var id: Int
+    var title: String
+    var video: String
+    var duration: String
+    var explanation: String
+}
+
+struct Test: Decodable {
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var questions: [Question]
+}
+
+struct Question: Decodable {
+    var id: Int
+    var content: String
+    var correctIndex: Int
+    var answers: [String]
+}
